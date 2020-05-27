ee85a19dea02f0bb1f704084a164fca09e7c43e9
Khaanh <35469637+Khaanh@users.noreply.github.com>
2020-05-28 02:42:05 +0300

    Add files via upload

---
+++ js/include/animation.js
@@ -0,0 +1,6 @@
+  // var animate = function anim() {
+  //   new WOW().init();
+  // }
+  // if (($(window).innerWidth()) > 1025) {
+  //   animate();
+  // }
---
+++ js/include/validate-form.js
@@ -0,0 +1,6 @@
+if ($('.form-control').length) {
+  $('.js-validCard').mask('0000 0000 0000 0000')
+  $('.js-validMY').mask('00/00')
+  $('.js-validCVC').mask('000')
+}
+
